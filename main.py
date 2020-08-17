23d71f3fbdd66980eea6d40301e644e4c8842abf
S8Vega <bsebastianvg18@gmail.com>
2020-08-16 19:32:53 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hola amigos!')
+
+
+def help_command(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('necesito ayuda!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    print(update.message.text)
+    update.message.reply_text(update.message.text)
+
+
+def sumar(update,context):
+    try:
+        numero1 = int(context.args[0])
+        numero2 = int(context.args[1])
+
+        suma = numero1 + numero2
+
+        update.message.reply_text("La suma es "+str(suma))
+
+    except (ValueError):
+        update.message.reply_text("por favor utilice dos numeros")
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1353141661:AAGT-RTJc6S7PbLmJsehDImSgP4H1O8K218", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
+
+
+    dp.add_handler(CommandHandler("sumar", sumar))
+
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
