96eebafbed149ec966260f7b88b05a130ff09df0
S8Vega <bsebastianvg18@gmail.com>
2020-08-20 22:22:37 -0500

    refactorizacion de codigo

--- calculadora.py
+++ calculadora.py
@@ -1,59 +1,57 @@
-class Calculadora:
-    def __init__(self):
-        self.st = []
+st = []
+
+def delim(c):
+    return c == ' '
+
+def is_op(c):
+    return c == '+' or c == '-' or c == '*' or c == '/'
+
+def priority(op):
+    if op == '+' or op == '-':
+        return 1
+    if op == '*' or op == '/':
+        return 2
+    return -1
+
+def process_op(op):
+    r = st.pop()
+    l = st.pop()
+    if op == '+':
+        st.append(l + r)
+    elif op == '-':
+        st.append(l - r)
+    if op == '*':
+        st.append(l * r)
+    if op == '/':
+        st.append(l // r)
     
-    def delim(self, c):
-        return c == ' '
-    
-    def is_op(self, c):
-        return c == '+' or c == '-' or c == '*' or c == '/'
-    
-    def priority(self, op):
-        if op == '+' or op == '-':
-            return 1
-        if op == '*' or op == '/':
-            return 2
-        return -1
-    
-    def process_op(self, op):
-        r = self.st.pop()
-        l = self.st.pop()
-        if op == '+':
-            self.st.append(l + r)
-        elif op == '-':
-            self.st.append(l - r)
-        if op == '*':
-            self.st.append(l * r)
-        if op == '/':
-            self.st.append(l // r)
-        
-    def evaluate(self, s):
-        self.st.clear()
-        op = []
-        i = 0
-        while i < len(s):
-            if self.delim(s[i]):
-                i += 1
-                continue
-            if s[i] == '(':
-                op.append(s[i])
-            elif s[i] == ')':
-                while op[-1] != '(':
-                    self.process_op(op.pop())
-                op.pop()
-            elif self.is_op(s[i]):
-                cur_op = s[i]
-                while len(op) > 0 and self.priority(op[-1]) >= self.priority(cur_op):
-                    self.process_op(op.pop())
-                op.append(cur_op)
-            else:
-                number = 0
-                while i < len(s) and s[i].isnumeric():
-                    number = number * 10 + int(s[i])
-                    i += 1
-                i -= 1
-                self.st.append(number)
+def evaluate(s):
+    st.clear()
+    op = []
+    i = 0
+    while i < len(s):
+        if delim(s[i]):
             i += 1
-        while len(op) > 0:
-            self.process_op(op.pop())
-        return self.st[-1]
+            continue
+        if s[i] == '(':
+            op.append(s[i])
+        elif s[i] == ')':
+            while op[-1] != '(':
+                process_op(op.pop())
+            op.pop()
+        elif is_op(s[i]):
+            cur_op = s[i]
+            while len(op) > 0 and priority(op[-1]) >= priority(cur_op):
+                process_op(op.pop())
+            op.append(cur_op)
+        else:
+            number = 0
+            while i < len(s) and s[i].isnumeric():
+                number = number * 10 + int(s[i])
+                i += 1
+            i -= 1
+            st.append(number)
+        i += 1
+    while len(op) > 0:
+        process_op(op.pop())
+    return st[-1]
--- main.py
+++ main.py
@@ -14,49 +14,34 @@ logger = logging.getLogger(__name__)
 TOKEN = os.getenv("TOKEN_TELEGRAM_CALCULATOR")
 
 def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hola amigos!')
+    update.message.reply_text('Hola amigos!!')
 
 
 def help_command(update, context):
-    """Send a message when the command /help is issued."""
     update.message.reply_text('necesito ayuda!')
 
 
 
 def calcular(update,context):
     try:
-        c = calculadora.Calculadora()
         s = ""
         for i in context.args:
             s = s + i
-        update.message.reply_text(f"La respuesta es {c.evaluate(s)}")
+        update.message.reply_text(f"La respuesta es {calculadora.evaluate(s)}")
 
     except (ValueError):
         update.message.reply_text("por favor utilice operaciones validas")
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
-
     dp.add_handler(CommandHandler("calcular", calcular))
-
-    # Start the Bot
+    
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    
     updater.idle()
 
 
