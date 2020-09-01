aa645b63010a648e448d6c0026815afaea8c6c24
S8Vega <bsebastianvg18@gmail.com>
2020-09-01 16:01:27 -0500

    agregando start y help

--- main.py
+++ main.py
@@ -16,14 +16,23 @@ TOKEN = os.getenv("TOKEN_TELEGRAM_CALCULATOR")
 def start(update, context):
     name = update.message.from_user.first_name
     logger.info(f"El usuario {name} ha iniciado una conversacion")
-    update.message.reply_text(f'Hola {name}')
+    mensaje = f"Hola {name}, soy una calculadora en Telegram, puedo resolver operaciones matematicas basicas, para mayor informacion utiliza /help"
+    update.message.reply_text(mensaje)
 
 
 def help_command(update, context):
-    update.message.reply_text('necesito ayuda!')
+    name = update.message.from_user.first_name
+    mensaje = f"""
+    Hola {name}, veo que necesitas ayuda, el comando para resolver las operaciones es /calcular seguido de la operacion que se desee realizar
+    por ejemplo:
+    /calcular 2+1
+    /calcular 2*(9-5)
+    /calcular 9*(999999999*18)-98
+    """
+    update.message.reply_text(mensaje)
 
 def valid(c):
-    return c.isdigit() or c == '+' or c == '-' or c == '*' or c == '/' 
+    return c.isdigit() or c == '+' or c == '-' or c == '*' or c == '/' or c == '(' or c == ')'
 
 def calcular(update,context):
     name = update.message.from_user.first_name
@@ -32,10 +41,15 @@ def calcular(update,context):
         for i in context.args:
             for c in i:
                 if(not valid(c)):
+                    print(c)
                     update.message.reply_text(f"{name} por favor utilice operaciones validas")
                     return
             s = s + i
-        update.message.reply_text(f"{name} la respuesta es {calculadora.evaluate(s)}")
+        print(s)
+        if len(s) == 0:
+            update.message.reply_text(f"{name} por favor digite una operacion valida")
+        else:
+            update.message.reply_text(f"{name} la respuesta es {calculadora.evaluate(s)}")
 
     except (ValueError):
         update.message.reply_text(f"{name} por favor utilice operaciones validas")
